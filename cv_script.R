4ea3ab739b4c375a86a50ddd1b28ef62a38fce9d
Murat Kaya <kozmikdalga@gmail.com>
2022-01-14 16:18:17 +0300

    İnit

---
+++ cv_script.R
@@ -0,0 +1,8 @@
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Murat Kaya",
+  data_location = "https://docs.google.com/spreadsheets/d/1nSr6Lx2D8eHBwU79iobgk41yMGfBb-zcCs_EWmvwsWI/edit?usp=sharing",
+  pdf_location = "https://github.com/cosmicwaves/CV/raw/master/kaya_cv.pdf",
+  html_location = "muratkaya.netlify.app/cv/",
+  source_location = "https://github.com/cosmicwaves/cv"
+)
