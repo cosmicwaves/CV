1587af938336362d3604b3e34a776d1d39f28bea
cosmicwaves <kozmikdalga@gmail.com>
2023-01-18 16:25:22 -0400

    script update

--- cv_script.R
+++ cv_script.R
@@ -1,4 +1,3 @@
-
 datadrivencv::use_datadriven_cv(
   full_name = "Murat Kaya",
   data_location = "https://docs.google.com/spreadsheets/d/1nSr6Lx2D8eHBwU79iobgk41yMGfBb-zcCs_EWmvwsWI/edit?usp=sharing",
